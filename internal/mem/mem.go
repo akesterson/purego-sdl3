c2a87dd96b3c24cc357a14d13bef43cc95c0cdd2
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2025-06-13 20:54:41 +0200

    internal DeepCopy function created

--- internal/mem/mem.go
+++ internal/mem/mem.go
@@ -12,3 +12,18 @@ func Copy[T any, N ~int32](array *T, count N) []T {
 	copy(result, unsafe.Slice(array, count))
 	return result
 }
+
+func DeepCopy[T any, N ~int32](array **T, count N) []*T {
+	if array == nil {
+		return nil
+	}
+	dst := make([]*T, count)
+	src := unsafe.Slice(array, count)
+	for i := 0; i < int(count); i++ {
+		if src[i] != nil {
+			value := *src[i]
+			dst[i] = &value
+		}
+	}
+	return dst
+}
